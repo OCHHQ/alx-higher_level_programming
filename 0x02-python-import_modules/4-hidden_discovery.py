e221f303264ca7e170dc6cb0c655c8f978644051
OCHHQ <enosejecollins@gmail.com>
2024-03-15 16:37:24 -0700

    4. Who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from hidden_4 import *
     allp = dir()
-    for i in range(10, len(allp)):
+    for i in range(0, len(allp)):
         if allp[i][:2] != "__":
             print("{:s}".format(allp[i]))
