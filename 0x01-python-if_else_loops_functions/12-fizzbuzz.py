057cd179ebbf0a6b63a96d1481f99c6d62f9b981
OCHHQ <enosejecollins@gmail.com>
2024-03-14 21:05:18 -0700

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 != 0:
+            print("FIZZ ", end='')
+        elif i % 5 == 0 and i % 3 != 0:
+            print("BUZZ", end='')
+        elif i % 5 == 0 and i % 3 == 0:
+            print("FIZZBUZZ", end='')
+        else:
+            print("{}".format(i), end='')
