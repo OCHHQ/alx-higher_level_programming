0cde54b562b9c51c37a24f7f669d348c7104f971
OCHHQ <enosejecollins@gmail.com>
2024-04-02 09:28:23 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""1-square module"""
+
+
+class Square:
+    """Square class with private size attribute"""
+
+    def __init__(self, size):
+        """Initialize Square with a private size attribute"""
+        self.__size = size
+
