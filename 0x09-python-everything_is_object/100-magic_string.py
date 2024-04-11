29bfb5a417f2bac4e3ae02cebc061cff2b8452d7
OCHHQ <enosejecollins@gmail.com>
2024-04-11 02:47:18 -0700

    Write a function magic_string() that returns a string “BestSchool” n times the number of the iteration (see code):

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
+def magic_string():i
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
     return "BestSchool" * magic_string.counter
-magic_string.counter = 0
-magic_string.counter += 1
