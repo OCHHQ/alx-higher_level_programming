476e7b09956fcd7d25cef2ce12745417cd3cb490
OCHHQ <enosejecollins@gmail.com>
2024-05-25 05:13:51 -0700

    11-genre_id_all_shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,10 +1,14 @@
+--
+-- 11-genre_id_all_shows.sql
+--
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+--
+
 SELECT 
-    ts.title, 
-    tsg.genre_id
+    ts.title, tsg.genre_id
 FROM 
     tv_shows ts
 LEFT JOIN 
     tv_show_genres tsg ON ts.id = tsg.show_id
 ORDER BY 
-    ts.title ASC, 
-    tsg.genre_id ASC;
+    ts.title ASC, tsg.genre_id ASC;
