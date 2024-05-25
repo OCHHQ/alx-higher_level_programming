2a825682261bd9252bb6d5801b11bd2161842060
OCHHQ <enosejecollins@gmail.com>
2024-05-25 05:09:09 -0700

    9-cities_by_state

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,14 @@
--- Select all cities with their respective states
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
-FROM cities, states
-WHERE cities.state_id = states.id
-ORDER BY cities.id;
+--
+-- 9-cities_by_state.sql
+--
+-- Lists all cities contained in the database hbtn_0d_usa.
+--
+
+SELECT 
+    c.id, c.name, s.name
+FROM 
+    cities c, states s
+WHERE 
+    c.state_id = s.id
+ORDER BY 
+    c.id ASC;
