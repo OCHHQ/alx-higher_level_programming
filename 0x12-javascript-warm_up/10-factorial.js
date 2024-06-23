df66a4d3ac01ef621b50447ea4de849a5cbc551f
OCHHQ <enosejecollins@gmail.com>
2024-06-23 00:09:01 -0700

    Add script that computes

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,19 @@
 #!/usr/bin/node
 
 function factorial(n) {
-  if (isNaN(n) || n === 0) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  } else if (n === 0) {
     return 1;
   } else {
-    return n * factorial(n - 1);
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+      result *= i;
+    }
+    return result;
   }
 }
 
 const arg = parseInt(process.argv[2]);
 console.log(factorial(arg));
+
