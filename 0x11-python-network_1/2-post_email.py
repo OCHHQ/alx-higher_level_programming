94b2565d091e6482b5db3fe275e9feef625f81ff
OCHHQ <enosejecollins@gmail.com>
2024-07-05 12:54:56 -0700

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script takes an email address as an argument, sends a POST request to
+(link unavailable)<email> and displays the
+response.
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+def email_valid(email):
+    """
+    This function sends a POST request to the given URL
+    """
+    url = "(link unavailable)"
+    data = urllib.parse.urlencode({"email": email})
+    data = data.encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        return response.read().decode("utf-8")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./2-post_email.py <email>")
+        sys.exit(1)
+
+    email = sys.argv[1]
+    response = email_valid(email)
+    print(response)
