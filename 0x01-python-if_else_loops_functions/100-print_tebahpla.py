36f1c953c26f58dcd675042da518b0bfca1d78e8
OCHHQ <enosejecollins@gmail.com>
2024-07-18 17:45:26 -0700

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 
-print("{}".format("".join([chr(122 - i) if i % 2 == 0 else chr(90 - i // 2) for i in range(26)])))
+print("{}" .format("".join([
+    chr(122 - i) if i % 2 == 0 else chr(90 - i // 2)
+    for i in range(26)
+])))
