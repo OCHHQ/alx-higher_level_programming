b3f5764067780b41e6d6bb7059e88788ac8d8ae2
Collins Enoseje <enosejecollins@gmail.com>
2024-07-31 19:02:19 +0100

    Add script to compute number of tasks completed by user ID

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (completedTasks[todo.userId] === undefined) {
+          completedTasks[todo.userId] = 0;
+        }
+        completedTasks[todo.userId]++;
+      }
+    });
+
+    console.log(completedTasks);
+  }
+});
